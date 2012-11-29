bbe4ce5dfeec6fb4010b11c45676ca9a22929892
terrencehan <hanliang1990@gmail.com>
2012-11-29 10:39:14 +0800

    add read_file()

--- lib/Automation.pm
+++ lib/Automation.pm
@@ -3,6 +3,7 @@ package Automation;
 use 5.014002;
 
 use Moose;
+use State;
 
 has 'states' => (
     is  => 'rw',
@@ -12,11 +13,31 @@ has 'states' => (
 sub read_file {
     my ( $self, $file_path ) = @_;
     open my $in, "<", $file_path or die "cannot open the transition table file";
+    my $first_line = <$in>;
+    my @symbols = split /\s+/, $first_line;
+    shift @symbols;    #remove 'state'
+
+    while (<$in>) {
+        my $count = 0;
+        my ($state_index) = $_ =~ /(\d+)/;
+        my $states = $self->states;
+        $states->[$state_index] ||= new State(num=>$state_index);
+        while (/{.*?}|#/g) {
+            my $entry = $&;
+            while ( $entry =~ /\d+/g ) { 
+                $states->[$state_index]->add_out_transition($symbols[$count] , $&);
+            }
+            $count++;
+        }
+
+        $self->states($states);
+    }
 }
-sub is_empty { }
-sub is_nfa   { }
-sub is_dfa   { }
-sub to_dfa   { }
-sub min_dfa  { }
+sub is_empty       { }
+sub is_nfa         { }
+sub is_dfa         { }
+sub to_dfa         { }
+sub min_dfa        { }
+sub get_states_num { }
 
 1;
--- t/Automation.t
+++ t/Automation.t
@@ -3,7 +3,22 @@ use strict;
 use warnings;
 use lib '../lib';
 
-use Test::More tests => 1;
+use Test::More 'no_plan';
 BEGIN { use_ok('Automation') };
 
 my $obj = new Automation;
+
+$obj->read_file('transition.table');
+
+use Data::Dump qw/dump/;
+my $res = (dump $obj->states)."\n";
+
+my $res_expect =<<END;
+[
+  bless({ num => 0, out_transition => { a => [0, 1], b => [0] } }, "State"),
+  bless({ num => 1, out_transition => { b => [2] } }, "State"),
+  bless({ num => 2, out_transition => { b => [3] } }, "State"),
+  bless({ num => 3 }, "State"),
+]
+END
+is $res, $res_expect;
