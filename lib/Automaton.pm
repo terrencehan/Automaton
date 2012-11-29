5a3b6b409f9e713b2165ef6a1f218cc5504f54b9
terrencehan <hanliang1990@gmail.com>
2012-11-29 22:19:05 +0800

    update

--- lib/Automaton.pm
+++ lib/Automaton.pm
@@ -1,3 +1,7 @@
+# lib/Automaton.pm
+# Copyright (c) 2012 terrencehan
+# hanliang1990@gmail.com
+
 package Automaton;
 
 use 5.014002;
@@ -5,19 +9,24 @@ use 5.014002;
 use Moose;
 use State;
 use GraphViz;
+use Text::Table;
 
 has 'states' => (
-    is  => 'rw',
-    isa => 'ArrayRef[State]',
+    is      => 'rw',
+    isa     => 'ArrayRef[State]',
+    default => sub { [] },
 );
 
 sub read_file {
     my ( $self, $file_path ) = @_;
     open my $in, "<", $file_path or die "cannot open the transition table file";
+
+    #get symbols
     my $first_line = <$in>;
     my @symbols = split /\s+/, $first_line;
     shift @symbols;    #remove 'state'
 
+    #deal with each line from the 2nd line
     while (<$in>) {
         my $count = 0;
         my ($state_index) = $_ =~ /(\d+)/;
@@ -39,12 +48,11 @@ sub read_file {
 sub as_png {
     my $self = shift;
     my $g = new GraphViz( rankdir => 'LR' );
-    for ( @{ $self->states } ) {
-        $g->add_node( $_->num );
-    }
+
+    $g->add_node( $_->num ) for ( @{ $self->states } );
 
     for my $state ( @{ $self->states } ) {
-        for my $label ( keys %{ $state->out_transition || {} } ) {
+        for my $label ( keys %{ $state->out_transition } ) {
             for my $to ( @{ $state->out_transition->{$label} } ) {
                 $g->add_edge( $state->num => $to, label => $label );
             }
@@ -53,11 +61,38 @@ sub as_png {
 
     $g->as_png;
 }
-sub is_empty       { }
-sub is_nfa         { }
-sub is_dfa         { }
-sub to_dfa         { }
-sub min_dfa        { }
-sub get_states_num { }
+
+sub get_states_num {
+    my $self = shift;
+    scalar( @{ $self->states } );
+}
+
+sub is_empty {
+    my $self = shift;
+    $self->get_states_num ? 0 : 1;
+}
+
+sub is_dfa {
+    my $self = shift;
+    for my $state ( @{ $self->states } ) {
+        for my $label ( keys %{ $state->out_transition } ) {
+            return 0 if scalar( @{ $state->out_transition->{$label} } ) > 1;
+        }
+    }
+
+    1;
+}
+
+sub is_nfa {
+    my $self = shift;
+    $self->is_dfa ? 0 : 1;
+}
+
+sub to_dfa   {
+    my $self = shift;
+    return if $self->is_dfa;
+}
+sub min_dfa  { }
+sub as_table { }
 
 1;
--- t/Automaton.t
+++ t/Automaton.t
@@ -7,10 +7,15 @@ use Test::More 'no_plan';
 #test 1
 BEGIN { use_ok('Automaton') };
 
-#test 2
 my $obj = new Automaton;
 
+#test 2
+is $obj->is_empty, 1;
+
 $obj->read_file('transition.table');
+
+#test 3
+is $obj->is_empty, 0;
 
 my $res = (dump $obj->states)."\n";
 
@@ -19,8 +24,12 @@ my $res_expect =<<END;
   bless({ num => 0, out_transition => { a => [0, 1], b => [0] } }, "State"),
   bless({ num => 1, out_transition => { b => [2] } }, "State"),
   bless({ num => 2, out_transition => { b => [3] } }, "State"),
-  bless({ num => 3 }, "State"),
+  bless({ num => 3, out_transition => {} }, "State"),
 ]
 END
+#test 4
 is $res, $res_expect;
-
+#test 5
+is $obj->is_nfa, 1;
+#test 6
+is $obj->is_dfa, 0;
